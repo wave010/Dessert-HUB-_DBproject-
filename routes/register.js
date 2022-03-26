6c27872d79f5e2a756af88ec05b97b0945b41c31
wave010 <thirawat.p@kkumail.com>
2022-03-27 05:28:44 +0700

    save

--- routes/register.js
+++ routes/register.js
@@ -5,6 +5,10 @@ const bodyParser = require('body-parser')
 router.get('/', function(req, res, next) {
     res.render("register");
   });
+router.post('/', function(req, res, next){
+  console.log(req.body)
+  res.send('helllo');
+});
 
-  
+
 module.exports = router;
