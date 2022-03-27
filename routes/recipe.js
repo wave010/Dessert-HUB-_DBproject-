3127c502f4370188f4f0d5bcaf593a2361538f22
TXRXX <toro2544@gmail.com>
2022-03-28 06:17:08 +0700

    Style recipe page

--- routes/recipe.js
+++ routes/recipe.js
@@ -1,20 +1,26 @@
-var express = require('express');
+var express = require("express");
+const { default: mongoose } = require("mongoose");
 const router = express.Router();
-const {ensureAuthenticated } = require('../config/auth');
+const { ensureAuthenticated } = require("../config/auth");
 
 //model recipe
-const recipe = require('../models/addrecipe');
+const recipe = require("../models/addrecipe");
+const user = require("../models/User");
 
-router.get('/', ensureAuthenticated ,function(req, res, next) {
-  recipe.find((err, recipe) =>{
-    if(!err){
-      res.render("recipe",{
-        recipe : recipe
-      });
-    }else{
-      console.log('Failed to retrueve')
-    }
-  })  
+
+router.get("/", ensureAuthenticated, function (req, res, next) {
+  recipe.find((err, recipe) => {
+    user.find((err, name) => {
+      if (!err) {
+        res.render("recipe", {
+          recipe: recipe,
+          name: name
+        });
+      } else {
+        console.log("Failed to retrueve");
+      }
+    });
   });
+});
 
 module.exports = router;
